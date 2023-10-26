2d08df28da55096002b02a1cba054d3b63224f51
lukas <lukas@172.16.98.135>
2023-10-26 12:37:00 +0200

    baking start

---
+++ src/Baking.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Baking {
+	public static void main(String [] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		String recipe; 
+		recipe = in.nextLine();
+		
+		if (recipe == 1) { 
+			
+		}
+		
+		
+		
+		
+		in.close(); 
+	}
+}
+
+/* 
+Level selector "what recipe" 
+Have like 3 levels with easy medium hard  
+
+Baking 
+- Ingredients into pot (Guessing game from list?) 
+- Mix (name random numbers? based on sth) 
+- bake > take out in the right time (for loop > ready true) (user input w/out waiting for them?) 
+- Done! 
+
+HP system? 
+> burned / cut self? 
+> what if there are enemies? 
+
+*/ 

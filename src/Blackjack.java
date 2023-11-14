0e0f0e1a8ad5d20fcc8320b78b0617914aedfabe
Luhrak <lukass@fn.de>
2023-11-15 00:14:19 +0100

    home lil thingy

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -12,9 +12,13 @@ public class Blackjack {
 		boolean exit = false;
 		int c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0;
 		int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0;
-		int pScore = c1+c2+c3+c4+c5;
-		int oScore = a1+a2+a3+a4+a5;
+		int pScore = 0;
+		int oScore = 0;
 		
+		/*
+		int pCalc ()) {
+			return c1+c2+c3+c4+c5; 
+		}*/ 
 
 		do {
 			while (!input.equals("d")) {
@@ -26,17 +30,20 @@ public class Blackjack {
 					c2 = (int) (Math.random() * 11 + 1);
 					a1 = (int) (Math.random() * 8 + 3);
 					a2 = (int) (Math.random() * 8 + 3);
+					pScore = c1+c2+c3+c4+c5;
+					oScore = a1+a2+a3+a4+a5;
 					System.out.println("Your cards: (total: " + pScore + ")  " + c1 + "  " + c2);
 					break;
 				}
 			}
 			while (!over) {
-				System.out.println("[d] draw card  [p] pass turn  [c] challenge  [f] forfit");
+				System.out.println("[d] draw card  [p] pass turn  [c] challenge  [f] forfeit");
 				input = into.nextLine();
 
 				if (input.equals("d")) {
 					// Problem: How to make adapt to 4 and 5
 					c3 = (int) (Math.random() * 11 + 1);
+					pScore = c1+c2+c3+c4+c5;
 					System.out.println("Your cards: (total: " + pScore + ")  " + c1 + "  " + c2 + "  " + c3);
 				}
 				if (input.equals("p")) {
@@ -70,7 +77,7 @@ public class Blackjack {
  * Welcome d to draw > get 2 random (always below 21) > Ai gets a random numer
  * (6-20)
  * 
- * while d to draw - p to pass - f to forfit - c to challenge/compare d > get 1
+ * while d to draw - p to pass - f to forfeit - c to challenge/compare d > get 1
  * random (1-11) each round ai might get another card or forfit f > wanna play
  * again? c > show all cards of player and ai announce winner wanna play again?
  * 

a2ecebd8be78273a3034c3fdda63b56cc1d72c04
bucccket <nicolas.lisgaras@gmail.com>
2023-11-15 02:04:10 +0100

    I just went off a bit Not really ideal Java, but it is the best you can do writing in a C style!

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -6,18 +6,37 @@ public class Blackjack {
 	private static int pCalc(int[] arr) {
 		return IntStream.of(arr).sum();
 	}
-	
+
 	private static int getRandomInt() {
 		return (int) (Math.random() * 11 + 1);
 	}
-	
-	public static void main(String[] args) {
 
+	private static void evaluate(int[] c, int[] a) {
+		int pScore = pCalc(c);
+		int oScore = pCalc(a);
+
+		if ((0 < pScore && pScore <= 21) && (0 < oScore && oScore <= 21)) {
+			if (pScore == oScore) {
+				System.out.println("Draw!");
+			} else if (pScore > oScore) {
+				System.out.println("You win!");
+			} else {
+				System.out.println("You lose!");
+			}
+		} else if (!(0 < pScore && pScore <= 21)) {
+			System.out.println("You lose");
+		} else if (!(0 < oScore && oScore <= 21)) {
+			System.out.println("Your opponent overshot! You win!");
+		} else {
+			System.out.println("Idk??? both lose??");
+		}
+		System.out.println("Your oppenent had these cards: (toal: " + (a[0] + a[1]) + ")  " + a[0] + "  " + a[1]);
+	}
+
+	public static void main(String[] args) {
 		Scanner into = new Scanner(System.in);
 		System.out.println("Welcome to Blackjack");
 
-		//HINT: variables that DONT need resetting can go outside the loop
-		//		variables that need resetting can go inside. that way they get reassigned every time
 		String input = "";
 		boolean exit = false;
 
@@ -26,70 +45,86 @@ public class Blackjack {
 			boolean over = false;
 			int[] c = new int[5];
 			int[] a = new int[5];
-			//int c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0;
-			//int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0;
 			int pScore = 0;
 			int oScore = 0;
+			int pPosition = 0;
+			int oPosition = 0;
+			int turns = 0;
 
-			// STEP 1 -
+			// STEP 1 - initial state
 			while (!input.equals("d")) {
 				System.out.println("To begin type [d] to draw 2 cards.");
 				input = into.nextLine();
 
 				if (input.equals("d")) {
-					c[0] = getRandomInt();
-					c[1] = getRandomInt();
-					a[0] = getRandomInt();
-					a[1] = getRandomInt();
+					c[pPosition++] = getRandomInt();
+					c[pPosition++] = getRandomInt();
+					a[oPosition++] = getRandomInt();
+					a[oPosition++] = getRandomInt();
 					pScore = pCalc(c);
 					oScore = pCalc(a);
 					System.out.println("Your cards: (total: " + pScore + ")  " + c[0] + "  " + c[1]);
-
-					/*
-					 * c1 = (int) (Math.random() * 11 + 1); c2 = (int) (Math.random() * 11 + 1); a1
-					 * = (int) (Math.random() * 8 + 3); a2 = (int) (Math.random() * 8 + 3); pScore =
-					 * c1+c2+c3+c4+c5; oScore = a1+a2+a3+a4+a5;
-					 * System.out.println("Your cards: (total: " + pScore + ")  " + c1 + "  " + c2);
-					 */
 					break;
 				}
 			}
 
-			// STEP 2 -
+			// STEP 2 - game loop
 			while (!over) {
 				System.out.println("[d] draw card  [p] pass turn  [c] challenge  [f] forfeit");
 				input = into.nextLine();
 
-				if (input.equals("d")) {
-					// Problem: How to make adapt to 4 and 5
-					
-					c[2] = getRandomInt();
-					pScore = pCalc(c);
-					System.out.println("Your cards: (total: " + pScore + ")  " + c[0] + "  " + c[1] + "  " + c[2]);
-					
-					//c3 = (int) (Math.random() * 11 + 1);
-					//pScore = c1 + c2 + c3 + c4 + c5;
-					//System.out.println("Your cards: (total: " + pScore + ")  " + c1 + "  " + c2 + "  " + c3);
-				}
-				if (input.equals("p")) {
-					// Problem: How to make adapt to 4 and 5
-					c[2] = 0;
-					//c3 = 0;
-				}
-				if (input.equals("c")) {
-					// Your cards
-					// opponents cards
-					// win / lose
+				switch (input) {
+
+				case "d":
+					if (pPosition < 5) {
+						c[pPosition++] = getRandomInt();
+						pScore = pCalc(c);
+					} else {
+						System.err.println("You already have 5 cards!");
+					}
+					System.out.flush(); // Soooo... since System.err and System.out are 2 different outputs they can
+										// draw over each other which can end up mangling text real badly
+					System.out.print("Your cards: (total: " + pScore + ")  ");
+					for (int card : c) {
+						if (card > 0) {
+							System.out.print(card + " ");
+						}
+					}
+					System.out.println();
+				case "p":
+					// 50% chance opponent draws card
+					if (Math.random() > 0.5 && oPosition < 5 && oScore < 21) {
+						a[oPosition++] = getRandomInt();
+					}
+					if (turns >= 5) {
+						System.out.println("Grrrr.. this takes too long. Im challenging!");
+						evaluate(c, a);
+						over = true;
+					}
+					break;
+
+				case "c":
+					evaluate(c, a);
 					over = true;
-				}
-				if (input.equals("f")) {
-					System.out.println(
-							"You lose. Your oppenent had these cards: (toal: " + (a[0] + a[1]) + ")  " + a[0] + "  " + a[1]);
+					break;
+
+				case "f":
+					System.out.println("You lose. Your oppenent had these cards: (toal: " + (a[0] + a[1]) + ")  " + a[0]
+							+ "  " + a[1]);
 					over = true;
+					break;
+
+				default:
+					System.err.println("Error! Invalid option");
 				}
+
+				// Update scores
+				pScore = pCalc(c);
+				oScore = pCalc(a);
+				turns++;
 			}
 
-			// STEP 3 -
+			// STEP 3 - Retry y / n?
 			System.out.println("Would you like to play again? (y/n)");
 			while (!input.equals("n") && !input.equals("y")) {
 				input = into.nextLine();

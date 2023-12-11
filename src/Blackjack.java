76f8e7f86ef2f772826bc67f288c2dd58e455e40
lukas <lukas@LuhSurface.fritz.box>
2023-12-11 12:09:13 +0100

    Finally finished the blackjack code hope there is no bugs

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -6,22 +6,18 @@ public class Blackjack {
 	public static void main(String[] args) {
 
 		try (Scanner into = new Scanner(System.in)) {
-			String input = "";
 			boolean exit = false;
-			//HINT: variables that DONT need resetting can go outside the loop
-			//		variables that need resetting can go inside. that way they get reassigned every time
+			int winstreak = 0;
 			
 			System.out.println("Welcome to Blackjack");
 	
+			// RESET TO INTIAL STATE -
 			while (!exit) {
-				// STEP 0 - RESET TO INTIAL STATE
-				boolean over = false;
-				int[] c = new int[5]; // dis a new int array with 5 "slots"
+				String input = "";
+				boolean gameActive = true;
+				int[] c = new int[5];
 				int[] a = new int[5];
-				//int c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0;
-				//int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0;
-				int pScore = 0;
-				int oScore = 0;
+				int round = 1;
 	
 				// STEP 1 -
 				while (!input.equals("d")) {
@@ -33,56 +29,69 @@ public class Blackjack {
 						c[1] = getRandomInt();
 						a[0] = getRandomInt();
 						a[1] = getRandomInt();
-						pScore = pCalc(c);
-						oScore = pCalc(a);
-						System.out.println("Your cards: (total: " + pScore + ")  " + c[0] + "  " + c[1]);
-	
-						/*
-						 * c1 = (int) (Math.random() * 11 + 1); c2 = (int) (Math.random() * 11 + 1); a1
-						 * = (int) (Math.random() * 8 + 3); a2 = (int) (Math.random() * 8 + 3); pScore =
-						 * c1+c2+c3+c4+c5; oScore = a1+a2+a3+a4+a5;
-						 * System.out.println("Your cards: (total: " + pScore + ")  " + c1 + "  " + c2);
-						 */
+						System.out.print("Your cards: ");
+						outPutHand(c, round);	
 						break;
 					}
 				}
 	
 				// STEP 2 -
-				while (!over) {
-					System.out.println("[d] draw card  [p] pass turn  [c] challenge  [f] forfeit");
-					input = into.nextLine();
-	
+				while (gameActive) {
+					round++;
+					
+					if (round == 5) {
+						System.out.println("Your opponent challenges you!");
+						System.out.print("They had these cards: ");
+						outPutHand(a, round);
+
+						boolean win = playerWin(c,a);
+						if (win) {winstreak++;}
+						gameActive = false;
+						break;
+					}
+					
+					input = getUserMove(into);
+
 					if (input.equals("d")) {
-						// Problem: How to make adapt to 4 and 5
+						c[round] = getRandomInt();
+						System.out.print("Your cards: ");
+						outPutHand(c, round);
 						
-						c[2] = getRandomInt();
-						pScore = pCalc(c);
-						System.out.println("Your cards: (total: " + pScore + ")  " + c[0] + "  " + c[1] + "  " + c[2]);
+					} else if (input.equals("p")) {
+						c[round] = 0;
+						System.out.print("Your cards: ");
+						outPutHand(c, round);
 						
-						//c3 = (int) (Math.random() * 11 + 1);
-						//pScore = c1 + c2 + c3 + c4 + c5;
-						//System.out.println("Your cards: (total: " + pScore + ")  " + c1 + "  " + c2 + "  " + c3);
+					} else if (input.equals("c")) {
+						System.out.println("You challenged your opponent.");
+						System.out.print("Your cards: ");
+						outPutHand(c, round);
+						System.out.print("Your opponent had these cards: ");
+						outPutHand(a, round);
+
+						boolean win = playerWin(c,a);
+						if (win) {winstreak++;}
+						gameActive = false;
+						
+					} else if (input.equals("f")) {
+						System.out.print("You forfeited. \nYour opponents cards: ");
+						outPutHand(a, round);	
+						gameActive = false;
 					}
-					if (input.equals("p")) {
-						// Problem: How to make adapt to 4 and 5
-						c[2] = 0;
-						//c3 = 0;
+					
+					// Opponent turn 
+					if (calcRes(a)< 19) {
+						a[round] = getRandomInt();
+					} else {
+						a[round] = 0;
 					}
-					if (input.equals("c")) {
-						// Your cards
-						// oppenments cards
-						// win / lose
-						over = true;
-					}
-					if (input.equals("f")) {
-						System.out.println(
-								"You lose. Your oppenent had these cards: (toal: " + (a[0] + a[1]) + ")  " + a[0] + "  " + a[1]);
-						over = true;
-					}
+					
+					
+					
 				}
 	
 				// STEP 3 -
-				System.out.println("Would you like to play again? (y/n)");
+				System.out.println("Your winstreak: " + winstreak + "\n\nWould you like to play again? (y/n)");
 				while (!input.equals("n") && !input.equals("y")) {
 					input = into.nextLine();
 					if (input.equals("n")) {
@@ -94,7 +103,56 @@ public class Blackjack {
 		}
 	}
 
-	private static int pCalc(int[] arr) {
+	
+	
+	private static boolean playerWin(int[] c, int[] a) {
+		int cScore = calcRes(c);
+		int aScore = calcRes(a);
+		
+		if (cScore > 21 && aScore > 21) {
+			System.out.print("Its a draw, you're both over 21. ");
+			return false;
+		} else if (cScore > 21) { 
+			System.out.print("You lose, your total is above 21. ");
+			return false;
+		} else if (aScore > 21) { 
+			System.out.print("You win, your opponents total is above 21. ");
+			return true;
+		} else if (cScore < aScore) {
+			System.out.print("You lose, your total is lower than your opponents. ");
+			return false;
+		} else {
+			System.out.print("You win, your total is higher than your opponents. ");
+			return true;
+		}
+		
+	}
+
+
+	public static void outPutHand(int[] b, int round) {
+		int Score = calcRes(b);
+		if (round == 6) {round = 5;}
+		
+		System.out.print("(total: " + Score + ") ");
+		
+		for (int i = 0; i<=round-1; i++) {
+			System.out.print("[" + b[i] + "] ");
+		}
+		if (round < 5) {
+		System.out.print("[" + b[round] + "] ");
+		}
+		System.out.println();
+	}
+
+
+	private static String getUserMove(Scanner into) {
+		System.out.println("[d] draw card  [p] pass turn  [c] challenge  [f] forfeit");
+		String input = into.nextLine();
+		return input;
+	}
+
+
+	private static int calcRes(int[] arr) {
 		return IntStream.of(arr).sum();
 	}
 
@@ -106,11 +164,6 @@ public class Blackjack {
 }
 
 
-/*
- * Array for player cards & for Ai cards 
- * function to draw a card > every time called gets counted up starting with 0
- * changes the int at the postion of that counting number in the array 
- */
 
 /*
  * ++Winstreak! Welcome d to draw > get 2 random (always below 21) > Ai gets a
